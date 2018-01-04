bbc928ad77ea6cd388419c814255b4d3c06a1c30
Atif Mahadik <atif@phantom.us>
2018-01-04 11:12:34 -0800

    PAPP-3996 Added copyright to the source code

--- wigle_connector.py
+++ wigle_connector.py
@@ -1,14 +1,22 @@
-# -----------------------------------------
-# Phantom sample App Connector python file
-# -----------------------------------------
+# --
+# File: wigle_connector.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
 
 # Phantom App imports
 import phantom.app as phantom
 from phantom.base_connector import BaseConnector
 from phantom.action_result import ActionResult
 
-# Usage of the consts file is recommended
-# from wigle_consts import *
 import requests
 import json
 from bs4 import BeautifulSoup
